71da9d6ce9ee69f9917bc44626dc7f12395f5f0c
Elvis Lam <elvis.hclam@yahoo.com.hk>
2020-04-30 18:30:11 +0800

    cal average version

--- read.py
+++ read.py
@@ -5,5 +5,11 @@ with open('reviews.txt', 'r') as f:
 		data.append(line)
 		count += 1
 		if count % 1000 == 0:
-		print(len(data))
+			print(len(data))
 print('Read Completed, You have', len(data), 'comment')
+
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+
+print('Average of the comment', sum_len/len(data))
